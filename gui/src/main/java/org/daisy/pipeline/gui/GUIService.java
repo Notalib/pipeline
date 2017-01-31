bd895b8ba999b6e81b5e8ec2a100161e19b840c3
Bert Frees <bertfrees@gmail.com>
2017-01-31 15:42:31 +0100

    [gui][modules/*] Use DS annotations instead of XML everywhere

--- gui/src/main/java/org/daisy/pipeline/gui/GUIService.java
+++ gui/src/main/java/org/daisy/pipeline/gui/GUIService.java
@@ -7,15 +7,25 @@ import org.daisy.pipeline.webserviceutils.storage.WebserviceStorage;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
 import org.osgi.framework.launch.Framework;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
+@Component(
+        name = "gui-service",
+        immediate = true
+)
 public class GUIService 
 {
         private static final Logger logger = LoggerFactory.getLogger(GUIService.class);
         private BundleContext ctxt;
 
+        @Activate
         public void init(BundleContext ctxt) {
                 this.ctxt=ctxt;
                 ServiceRegistry.getInstance().setGUIService(this);
@@ -41,18 +51,53 @@ public class GUIService
 
 
 
+        @Reference(
+                name = "script-registry",
+                unbind = "-",
+                service = ScriptRegistry.class,
+                cardinality = ReferenceCardinality.MANDATORY,
+                policy = ReferencePolicy.STATIC
+        )
         public void setScriptRegistry(ScriptRegistry scriptRegistry) {
                 ServiceRegistry.getInstance().setScriptRegistry(scriptRegistry);
         }
+        @Reference(
+                name = "job-manager-factory",
+                unbind = "-",
+                service = JobManagerFactory.class,
+                cardinality = ReferenceCardinality.MANDATORY,
+                policy = ReferencePolicy.STATIC
+        )
         public void setJobManagerFactory(JobManagerFactory jobManagerFactory) {
                 ServiceRegistry.getInstance().setJobManagerFactory(jobManagerFactory);
         }
+        @Reference(
+                name = "event-bus-provider",
+                unbind = "-",
+                service = EventBusProvider.class,
+                cardinality = ReferenceCardinality.MANDATORY,
+                policy = ReferencePolicy.STATIC
+        )
         public void setEventBusProvider(EventBusProvider eventBusProvider) {
                 ServiceRegistry.getInstance().setEventBusProvider(eventBusProvider);
         }
+        @Reference(
+                name = "webservice-storage",
+                unbind = "-",
+                service = WebserviceStorage.class,
+                cardinality = ReferenceCardinality.MANDATORY,
+                policy = ReferencePolicy.STATIC
+        )
         public void setWebserviceStorage(WebserviceStorage webserviceStorage) {
                 ServiceRegistry.getInstance().setWebserviceStorage(webserviceStorage);
         }
+        @Reference(
+                name = "datatype-registry",
+                unbind = "-",
+                service = WebserviceStorage.class,
+                cardinality = ReferenceCardinality.MANDATORY,
+                policy = ReferencePolicy.STATIC
+        )
         public void setDatatypeRegistry(DatatypeRegistry datatypeRegistry) {
                 ServiceRegistry.getInstance().setDatatypeRegistry(datatypeRegistry);
         }
