c3310a1adfb219a7734acec0a0ee3769e3b2c9fe
Bert Frees <bertfrees@gmail.com>
2017-02-02 17:46:34 +0100

    [build-utils] modules-test-helper: use TestRunner in AbstractTest
    System properties can be specified and are applied both with and without
    OSGi.
    
    Tested in modules-build-helper.

--- utils/build-utils/modules-build-helper/src/it/it-pipeline-module/src/test/java/ServicesTest.java
+++ utils/build-utils/modules-build-helper/src/it/it-pipeline-module/src/test/java/ServicesTest.java
@@ -3,28 +3,12 @@ import javax.inject.Inject;
 import org.daisy.pipeline.datatypes.DatatypeService;
 import org.daisy.pipeline.script.XProcScriptService;
 
-import org.daisy.pipeline.junit.TestRunner;
-
-import static org.daisy.pipeline.pax.exam.Options.felixDeclarativeServices;
-import static org.daisy.pipeline.pax.exam.Options.logbackClassic;
-import static org.daisy.pipeline.pax.exam.Options.logbackConfigFile;
-import static org.daisy.pipeline.pax.exam.Options.mavenBundle;
-import static org.daisy.pipeline.pax.exam.Options.mavenBundlesWithDependencies;
-import static org.daisy.pipeline.pax.exam.Options.thisBundle;
+import org.daisy.pipeline.junit.AbstractTest;
 
 import static org.junit.Assert.assertEquals;
-import org.junit.runner.RunWith;
 import org.junit.Test;
 
-import org.ops4j.pax.exam.Configuration;
-import static org.ops4j.pax.exam.CoreOptions.junitBundles;
-import static org.ops4j.pax.exam.CoreOptions.options;
-import org.ops4j.pax.exam.Option;
-import org.ops4j.pax.exam.ProbeBuilder;
-import org.ops4j.pax.exam.TestProbeBuilder;
-
-@RunWith(TestRunner.class)
-public class ServicesTest {
+public class ServicesTest extends AbstractTest {
 	
 	@Inject
 	public DatatypeService datatype;
@@ -46,23 +30,16 @@ public class ServicesTest {
 	/* For OSGi only */
 	/* ------------- */
 	
+	@Override
+	protected String[] testDependencies() {
+		return new String[]{
+			"org.daisy.pipeline:framework-core:?"
+		};
+	}
+	
 	@ProbeBuilder
 	public TestProbeBuilder probeConfiguration(TestProbeBuilder probe) {
 		probe.setHeader("Service-Component", "OSGI-INF/resolver-mock.xml");
 		return probe;
-	}
-	
-	@Configuration
-	public Option[] config() {
-		return options(
-			logbackConfigFile(),
-			felixDeclarativeServices(),
-			thisBundle(),
-			junitBundles(),
-			mavenBundlesWithDependencies(
-				// logging
-				logbackClassic(),
-				mavenBundle("org.daisy.pipeline:framework-core:?"))
-		);
 	}
 }
